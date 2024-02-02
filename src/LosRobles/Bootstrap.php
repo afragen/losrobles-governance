b2a38cf1c6610270acc1ad16df3e580e8a498eef
Andy Fragen <andy@thefragens.com>
2024-02-02 11:35:44 -0800

    add_user_roles befor add_cap

--- src/LosRobles/Bootstrap.php
+++ src/LosRobles/Bootstrap.php
@@ -7,8 +7,8 @@ class Bootstrap {
 	public function run() {
 		$this->add_user_roles();
 		$this->add_extra_admin_caps();
-		//$this->add_caps_board_member();
-		//$this->add_caps_member_noprivs();
+		$this->add_caps_board_member();
+		$this->add_caps_member_noprivs();
 		$this->init_voting();
 		( new Base() )->load_hooks();
 	}
@@ -32,6 +32,11 @@ class Bootstrap {
 			)
 		);
 		$roles->add_role(
+			'board_member',
+			'Board Member',
+			array()
+		);
+		$roles->add_role(
 			'non_members',
 			'Non-Members',
 			array(
@@ -40,54 +45,33 @@ class Bootstrap {
 			)
 		);
 		$roles->add_role(
-			'board_member',
-			'Board Member',
-			array(
-				'read'                 => true,
-				'can_vote'             => true,
-				'edit_lrhoa_fields'    => true,
-				'email_single_user'    => true,
-				'email_multiple_users' => true,
-				'email_user_groups'    => true,
-			)
-		);
-		$roles->add_role(
 			'member_noprivs',
 			'Member No Privileges',
-			array(
-				'read'     => true,
-				'can_vote' => false,
-			)
+			array()
 		);
 	}
 
 	private function add_extra_admin_caps() {
-		$role = $this->get_role('administrator');
-		//$role = get_role( 'administrator' );
+		if ( is_null( get_role( 'administrator' ) ) ) {
+			require_once ABSPATH . 'wp-admin/includes/schema.php';
+			populate_roles();
+		}
+		$role = get_role( 'administrator' );
 		$role->add_cap( 'edit_lrhoa_fields' );
 		$role->add_cap( 'members' );
 		$role->add_cap( 'board_member' );
 	}
 
 	private function add_caps_board_member() {
-		$role = $this->get_role('board_member');
-		//$role = get_role( 'board_member' );
+		$this->add_user_roles();
+		$role = get_role( 'board_member' );
 		$role->add_cap( 'members' );
 	}
 
 	private function add_caps_member_noprivs() {
-		$role = $this->get_role('member_noprives');
-		//$role = get_role( 'member_noprivs' );
+		$this->add_user_roles();
+		$role = get_role( 'member_noprivs' );
 		$role->add_cap( 'non_members' );
-	}
-
-	// Fixes PHP Fatal error Uncaught Error: Call to a member function add_cap() on null.
-	private function get_role($role) {
-		if ( is_null(get_role($role))){
-		require_once ABSPATH . 'wp-admin/includes/schema.php';
-		populate_roles();
-		}
-		return get_role($role);
 	}
 
 	private function init_voting() {
